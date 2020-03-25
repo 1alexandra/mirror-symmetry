scaling bug fix, get_drawing args bug fix with get_contours changing, fix_period instead of add_middles

zhu_contour.py
@@ -38,7 +38,7 @@ def binarize(image):
                                     5, 0)
     return img
     
-def get_contours(path, get_all = False):
+def get_contours(path, get_all = False, min_area = 50):
     """
     input:
     image -- cv2 read bw image,
@@ -50,37 +50,79 @@ def get_contours(path, get_all = False):
     contours, _ = cv2.findContours(img,
                                    cv2.RETR_EXTERNAL,
                                    cv2.CHAIN_APPROX_NONE)
-    if not get_all:
-        measure = [cv2.contourArea(cnt) for cnt in contours]
-        contours = [contours[np.argmax(measure)]]
-    return [cnt_to_u(cnt) for cnt in contours]
+    measure = np.array([cv2.contourArea(cnt) for cnt in contours])
+    print(measure)
+    if get_all:
+        index = np.arange(len(measure))[measure >= min_area]
+    else:
+        index = [np.argmax(measure)]
+    return [cnt_to_u(contours[i]) for i in index]
 
-def preprocess(u_, mid_iters = 1):
+def fix_period(u, n_mult=2):
+    """
+    input:
+    u -- complex array, contour points,
+    n -- number of output contour points, if None, n = len(u)
+    output:
+    w -- complex array, contour points with equal paths along the contour
+        between each adjacent pair
+    """
+    n = len(u) * n_mult
+    u_next = np.zeros(u.shape, dtype=complex)
+    u_next[:-1] = u[1:]
+    u_next[-1] = u[0]
+    step = np.sum(np.abs(u-u_next)) / n
+    seg_ind = 0
+    seg_start = u[0]
+    cur_step = step
+    w = []
+    for i in range(n):
+        w.append(seg_start)
+        while True:
+            seg_end = u[(seg_ind+1) % len(u)]
+            seg_vec = seg_end - seg_start
+            seg_len = abs(seg_vec)
+            if seg_len < cur_step:
+                seg_ind += 1
+                seg_start = seg_end
+                cur_step -= seg_len
+            else:
+                seg_start += seg_vec / seg_len * cur_step
+                cur_step = step
+                break
+    return np.array(w)
+
+def add_middles(u, mid_iters=1):
     """
     input: 
     u_ -- complex array, contour points,
     mid_iters -- int,
     output:
-    preprocessed u, comlex array:
-        max(abs(u)) = 1,
-        min(re(u)) = min(im(u)) = 0;
-        'mid_iters' times added contour edge centers,
-    vec -- complex,
-    scale -- double,
-    u = (u-vec)/scale.
+    preprocessed u, comlex array 'mid_iters' times added contour edge centers
     """
-    u = u_.copy()
-    vec = np.min(np.real(u)) + np.min(np.imag(u)) * 1j
-    scale = np.max(np.abs(u))
-    u -= vec
-    u /= scale
     u_m = []
     parts = 2 ** mid_iters
     steps = (1/parts) * np.arange(parts)
     for i in range(len(u)):
         cur = u[(i+1)%len(u)] - u[i]
         u_m += list(u[i] + steps * cur)
-    return np.array(u_m), vec, scale
+    return np.array(u_m)    
+        
+def preprocess(u):
+    """
+    input: 
+    u_ -- complex array, contour points,
+    output:
+    preprocessed u, comlex array:
+        max(abs(u)) = 1,
+        min(re(u)) = min(im(u)) = 0;
+    vec -- complex,
+    scale -- double,
+    u = (u-vec)/scale.
+    """
+    vec = np.min(np.real(u)) + np.min(np.imag(u)) * 1j
+    scale = np.max(np.abs(u-vec))
+    return (u-vec) / scale, vec, scale
 
 def preprocess_inverse(u, vec, scale):
     return u * scale + vec
@@ -98,5 +140,5 @@ def index_neighbors(u, z, delta = 5):
         return np.arange(len(u))
     u_step = np.array(list(u[1:])+[u[0]])
     tmp = max(np.min(np.abs(u_step-u)),1)
-    delta_new = delta * tmp ### было /
+    delta_new = delta * tmp
     return np.arange(len(u))[np.abs(u-z) <= delta_new]

zhu_symmetry.py
@@ -3,8 +3,7 @@ import numpy as np
 import cv2
 from scipy.spatial import ConvexHull
 
-from zhu_contour import preprocess, preprocess_inverse, index_neighbors 
-from zhu_contour import u_to_cnt, cnt_to_u, get_contours
+from zhu_contour import *
 
 def nearest_to_line(u, z1, z2):
     """
@@ -21,7 +20,6 @@ def nearest_to_line(u, z1, z2):
                    - (re_z2 - re_z1) * np.imag(u)
                    + im_z1 * re_z2 - im_z2 * re_z1)
     return np.argmin(dists)
-
 
 def hull_based_index(u, delta = None):
     """
@@ -51,7 +49,7 @@ def hull_based_index(u, delta = None):
         by_hull += list(index_neighbors(u, u[ind], delta))
     by_hull = np.unique(np.ravel(by_hull))
     return by_hull, hull_ind, middles
-    
+
 def f_abs_based_index(f, alpha = 0, beta = 1, ret_zero = False):
     """
     input:
@@ -76,7 +74,7 @@ def f_abs_based_index(f, alpha = 0, beta = 1, ret_zero = False):
     crit_1[0] = ret_zero
     crit_2 = np.logical_or(ind <= garms/2, ind >= N-garms/2)
     return ind[crit_1 * crit_2], eps, garms
-    
+
 def find_theta(dots):
     """
     input:
@@ -92,7 +90,7 @@ def find_theta(dots):
     f = lambda t: k1*np.cos(t)**2 + k2*np.sin(t)**2 + 2*k3*np.sin(t)*np.cos(t)
     if k1 == k2:
         if k3 == 0:
-            print('find_theta error')
+            print('find_theta error', dots)
             return 0
         t1 = np.pi/4
     elif k3 == 0:
@@ -107,7 +105,7 @@ def find_theta(dots):
         if f(0) > f(t2):
             return - t2 % np.pi
     return 0
-    
+
 def measure_axis(dots, N): 
     """
     input:
@@ -121,9 +119,6 @@ def measure_axis(dots, N):
     theta = find_theta(dots)
     b = np.imag(dots * np.exp(-1j*theta))
     # было (np.sum(b*b)/N)**0.5
-    ### TODO может быть нормировать на количество точек?
-    ### TODO может быть нормировать на max(abs(dot))?
-    ### TODO может быть смотреть не на квадрат мнимой части, а на модуль?
     return np.sum(b*b)**0.5 / N, theta
 
 def new_start_point(f, s, ind = None):
@@ -137,40 +132,42 @@ def new_start_point(f, s, ind = None):
     """
     N = len(f)
     ind = ind if (ind is not None) else np.arange(N)
-    return f[ind] * np.exp(-1j * 2*np.pi/N * ind * (N-s)) ### было s   
+    return f[ind] * np.exp(-1j * 2*np.pi/N * ind * (N-s))  
 
 def find_sym(u_, 
              delta_hull = None, 
              alpha = 0, 
              beta = 1, 
              delta_neib = None, 
-             q_th = np.inf
+             q_th = np.inf, 
+             find_all = False
             ):
-    u, vec, scale = preprocess(u_)
+    u, vec, scale = preprocess(fix_period(u_))
     f = np.fft.fft(u)
     N = len(u)
-    by_hull, *_ = hull_based_index(u, delta_hull)
-    f_ind, *_ = f_abs_based_index(f, alpha, beta)
-    qs1 = [measure_axis(new_start_point(f, s, f_ind), N)[0] 
-           for s in by_hull]
-    approx_ind = by_hull[np.argmin(qs1)]
-    neibs = index_neighbors(u, u[approx_ind], delta_neib)
-    all_f_ind = np.arange(1,N)
-    qs2 = [measure_axis(new_start_point(f, s, all_f_ind), N)[0] 
-           for s in neibs]
-    sym_ind = neibs[np.argmin(qs2)]
-    q, theta = measure_axis(new_start_point(f, sym_ind, all_f_ind), N)
-    sym_point, sym_vec = None, None
-    if q <= q_th:
-        sym_point = preprocess_inverse(u[sym_ind], vec, scale)
-        sym_vec = np.exp(1j*theta)
-    return q, (sym_point, sym_vec) 
-    
+    if not find_all:
+        by_hull, *_ = hull_based_index(u, delta_hull)
+        f_ind, *_ = f_abs_based_index(f, alpha, beta)
+        qs1 = [measure_axis(new_start_point(f, s, f_ind), N)[0] 
+               for s in by_hull]
+        approx_ind = by_hull[np.argmin(qs1)]
+        neibs = index_neighbors(u, u[approx_ind], delta_neib)
+        all_f_ind = np.arange(1,N)
+        qs2 = [measure_axis(new_start_point(f, s, all_f_ind), N)[0] 
+               for s in neibs]
+        sym_ind = neibs[np.argmin(qs2)]
+        q, theta = measure_axis(new_start_point(f, sym_ind, all_f_ind), N)
+        sym_point, sym_vec = None, None
+        if q <= q_th:
+            sym_point = preprocess_inverse(u[sym_ind], vec, scale)
+            sym_vec = np.exp(1j*theta)
+        return q, (sym_point, sym_vec)
+
 def get_drawing_args(folder):
     drawing_args = []
     for name in os.listdir(path='./'+folder):
         try:
-            u_list = get_contours(folder+'/'+name)
+            u_list = get_contours(folder+'/'+name, True)
         except BaseException:
             print(folder+'/'+name, 'error')
             continue